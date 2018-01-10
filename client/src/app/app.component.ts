2e41fa2c6c8abf2466652e0f3e58b5c840f226e9
Adam Herrmann <adamherrmann13@gmail.com>
2018-01-09 23:25:00 -0500

    styling updates

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry} from '@angular/material/icon';
 
 
 @Component({
@@ -10,10 +12,31 @@ import { MatSidenav } from '@angular/material/sidenav';
 export class AppComponent {
   @ViewChild('sidenav') sidenav: MatSidenav;
   title = 'Detroit Harps';
+  facebookUrl = 'https://www.facebook.com/DetroitHarpsYouthGFC/';
+  twitterUrl = 'https://twitter.com/DetroitHarps';
+  instagramUrl = 'https://www.instagram.com/detroitharps/';
+  shopUrl = 'https://www.oneills.com/shop-by-team/gaa/usa/detroit-harps-gaa.html';
 
-  reason = '';
-  close(reason: string) {
-    this.reason = reason;
+  constructor(private _mdIconRegistry: MatIconRegistry, 
+    private _sanitizer: DomSanitizer) {
+        _mdIconRegistry
+            .addSvgIcon('facebook',
+            _sanitizer.bypassSecurityTrustResourceUrl('assets/icons/facebook.svg'));
+        _mdIconRegistry
+            .addSvgIcon('twitter',
+            _sanitizer.bypassSecurityTrustResourceUrl('assets/icons/twitter.svg'));
+        _mdIconRegistry
+            .addSvgIcon('instagram',
+            _sanitizer.bypassSecurityTrustResourceUrl('assets/icons/instagram.svg'));
+        _mdIconRegistry
+            .addSvgIcon('shop',
+            _sanitizer.bypassSecurityTrustResourceUrl('assets/icons/shop.svg'));
+        _mdIconRegistry
+            .addSvgIcon('logo',
+            _sanitizer.bypassSecurityTrustResourceUrl('assets/icons/detroitharpslogo.svg'));
+  }
+
+  close() {
     this.sidenav.close();
   }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -33,6 +33,7 @@ import { ContactComponent } from './contact/contact.component'
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
