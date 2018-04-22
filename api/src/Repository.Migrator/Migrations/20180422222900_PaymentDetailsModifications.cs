8270d4ae701f4edb6f149c8f28474225cef2a6fe
Adam Herrmann <adamherrmann13@gmail.com>
2018-04-22 18:30:12 -0400

    modifications to payment details to track multiple payment types

---
+++ api/src/Repository.Migrator/Migrations/20180422222900_PaymentDetailsModifications.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Repository.Migrator.Migrations
+{
+    public partial class PaymentDetailsModifications : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentType",
+                table: "PaymentDetails",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "VerfiedPayment",
+                table: "PaymentDetails",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentType",
+                table: "PaymentDetails");
+
+            migrationBuilder.DropColumn(
+                name: "VerfiedPayment",
+                table: "PaymentDetails");
+        }
+    }
+}
