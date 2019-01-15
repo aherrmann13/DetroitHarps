4ed6735f05a672a22a842d65bccebe3a7e059962
Adam Herrmann <adamherrmann13@gmail.com>
2019-01-14 22:54:02 -0500

    circleci config added

---
+++ .circleci/run-build-locally.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+COMMIT_HASH="caab2768ce7b88dd3625fda68303c52cc65e6540"
+curl --user ${CIRCLE_TOKEN}: \
+    --request POST \
+    --form revision=$COMMIT_HASH\
+    --form config=@config.yaml \
+    --form notify=false \
+        https://circleci.com/api/v1.1/project/github/aherrmann13/DetroitHarps/tree/dev
