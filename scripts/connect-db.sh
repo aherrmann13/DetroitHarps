f14bf58951fa9c3075b85349f40969aa69e2d62c
Adam Herrmann <adamherrmann13@gmail.com>
2019-01-14 23:46:51 -0500

    dev database connection strings

--- scripts/connect-db.sh
+++ scripts/connect-db.sh
@@ -1,2 +1,2 @@
 ## all test env stuff, can be checked in
-psql --host=detroitharps-dev.cx2yqn0s5zfm.us-east-2.rds.amazonaws.com --port=5432 --username=detroitharps --password --dbname=detroitharps_dev
+psql --host=157.230.51.199 --port=5432 --username=detroitharps --password --dbname=detroitharps
