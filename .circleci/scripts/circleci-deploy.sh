fe39c861ee1d74a82fe77d6a68e75efa203ee3db
Adam Herrmann <adamherrmann13@gmail.com>
2019-01-31 23:22:16 -0500

    fingerprint automatically added and config.yml formatting

--- .circleci/scripts/circleci-deploy.sh
+++ .circleci/scripts/circleci-deploy.sh
@@ -3,14 +3,14 @@
 DEPLOY_FOLDER="/detroitharps/publish"
 if [ "${CIRCLE_BRANCH}" == "dev" ]
 then
-  PUBLIC_KEY="AAAAB3NzaC1yc2EAAAADAQABAAABAQCaXdHcq9Ydf4H9E1jw9ydFKG81TKf5+P5LPMQmTR9zuvHSlm/4ZLnPSM+yzi6oceqU/QbwxhXefi/Mq6DhjNArCnKcWND1ty04uDiF7/XwqGDxWy8HWW1thji8CH+mxWt3TZoI11ZBGYr+O+0+O8Chp0vAAUueggeygKOwJVXswO7kaJGM2RVMoRpZholi8Pj1fcoAitwoW3Gjzw1005GQcTG7w0j5Nrtvd3Gnrlznyql80TWxpSF6UGlxqjbM+r1EzIwwe8XmeaIg/DqeNWii28w+seknj3IKtRGAAqJxeue3HCQ35xbpTZLykR9bTgHIpY4WjnQebn+BT36S5sod"
-  SSH_ADDR="detroitharps@157.230.51.199"
+  IP_ADDRESS="157.230.51.199"
+  SSH_ADDR="detroitharps@$IP_ADDRESS"
 else
   echo "no settings for branch: ${CIRCLE_BRANCH}"
   exit 1
 fi
 
-echo $PUBLIC_KEY >> ~/.ssh/known_hosts
+ssh-keyscan -H $IP_ADDRESS >> ~/.ssh/known_hosts
 
 ssh $SSH_ADDR "if [ -d $DEPLOY_FOLDER ]; then rm -rf $DEPLOY_FOLDER; fi"
 ssh $SSH_ADDR "mkdir -p $DEPLOY_FOLDER"
