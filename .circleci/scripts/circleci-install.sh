276734698524eca4f6221a3d5af745bc593b4876
Adam Herrmann <adamherrmann13@gmail.com>
2019-01-31 23:46:56 -0500

    fixed issue copying files in install script

--- .circleci/scripts/circleci-install.sh
+++ .circleci/scripts/circleci-install.sh
@@ -15,6 +15,6 @@ ssh-keyscan -H $IP_ADDRESS >> ~/.ssh/known_hosts
 
 ssh $SSH_ADDR "if [ -d $RUN_FOLDER ]; then rm -rf $RUN_FOLDER; fi"
 ssh $SSH_ADDR "mkdir -p $RUN_FOLDER"
-ssh $SSH_ADDR "cp -r $DEPLOY_FOLDER $RUN_FOLDER"
+ssh $SSH_ADDR "cp -r $DEPLOY_FOLDER/* $RUN_FOLDER/"
 
 exit
