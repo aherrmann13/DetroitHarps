bf2681fd7377921f8fe3dfe179edcf7d299ca450
Adam Herrmann <adamherrmann13@gmail.com>
2019-02-05 00:24:23 -0500

    moving all deployment logic to circleci config

--- .circleci/run-build-locally.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-COMMIT_HASH=$(git ls-remote git://github.com/aherrmann13/DetroitHarps.git refs/heads/dev | cut -f 1)
-curl --user ${CIRCLE_TOKEN}: \
-    --request POST \
-    --form revision=$COMMIT_HASH\
-    --form config=@config.yml \
-    --form notify=false \
-        https://circleci.com/api/v1.1/project/github/aherrmann13/DetroitHarps/tree/dev
