df619cbf2e6ca4313824eaa3b264c4a6aff607b0
Adam Herrmann <adamherrmann13@gmail.com>
2019-03-05 20:56:41 -0500

    fixed client on admin registration page

--- src/Client/src/app/admin/registration/registration.component.ts
+++ src/Client/src/app/admin/registration/registration.component.ts
@@ -1,17 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
 
 import {
-  Client,
-  ContactModel,
-  UserCredentialsModel,
-  RegistrationReadModel,
-  ChildInformationReadModel
+  RegisteredChildModel,
+  Client
 } from '../../shared/client/api.client';
-import { Router } from '@angular/router';
-
-import { IRegistrationInformationModel } from './registration.model';
-import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/observable/forkJoin';
 
@@ -21,34 +13,23 @@ import 'rxjs/add/observable/forkJoin';
   templateUrl: './registration.component.html',
   styleUrls: [ ]
 })
+
 export class RegistrationComponent implements OnInit {
-  info: IRegistrationInformationModel[];
-  columnsToDisplay: string[] = ['parentName', 'childName', 'emailAddress', 'gender', 'dateOfBirth', 'shirtSize'];
+  info: RegisteredChildModel[];
+  columnsToDisplay: string[] = [
+    'parentName',
+    'childName',
+    'gender',
+    'emailAddress',
+    'dateOfBirth',
+    'shirtSize'];
 
-  constructor(private _client: Client, private _router: Router) { }
+  constructor(private _client: Client) { }
+
   ngOnInit() {
-
-    const parentsObservable = this._client.getAllRegistered();
-    const childObservable = this._client.getAllRegisteredChildren();
-
-    Observable.forkJoin([parentsObservable, childObservable]).subscribe(
-      data => this.ProcessRegistration(data),
+    this._client.getAllChildren().subscribe(
+      data => this.info = data,
+      error => console.error(error)
     );
-  }
-
-  private ProcessRegistration(data: any): void {
-    const parents = data['0'] as RegistrationReadModel[];
-    const children = data['1'] as ChildInformationReadModel[];
-    this.info = children.map(function(x) {
-      const parent = parents.find(y => y.id === x.parentId);
-      return {
-        parentName: parent.firstName + ' ' + parent.lastName,
-        childName: x.firstName + ' ' + x.lastName,
-        emailAddress: parent.emailAddress,
-        gender: x.gender,
-        dateOfBirth: x.dateOfBirth,
-        shirtSize: x.shirtSize
-      };
-    });
   }
 }
--- src/Client/src/app/admin/registration/registration.model.ts
+++
@@ -1,8 +0,0 @@
-export interface IRegistrationInformationModel {
-    parentName?: string;
-    emailAddress?: string;
-    childName?: string;
-    gender?: string;
-    dateOfBirth?: Date;
-    shirtSize?: string;
-}
