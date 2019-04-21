248d99c233081c676eb5d025180e38c9f6ce86bf
Adam Herrmann <adamherrmann13@gmail.com>
2019-04-20 20:20:04 -0400

    adding mobile sizing for footer

--- src/Client/src/app/site/footer/footer.component.ts
+++ src/Client/src/app/site/footer/footer.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'dh-footer',
-  templateUrl: './footer.component.html'
+  templateUrl: './footer.component.html',
+  styleUrls: [ './footer.component.scss']
 })
 export class FooterComponent {
   shopUrl = 'https://www.oneills.com/shop-by-team/gaa/usa/detroit-harps-gaa.html';
