cb262d056674da3e4ff0d2bd75c5886b0b8f3a4f
Adam Herrmann <adamherrmann13@gmail.com>
2021-04-11 15:12:15 -0400

    fixing bearer token not getting added to http request

--- src/Client/src/app/core/client/api.client.base.ts
+++ src/Client/src/app/core/client/api.client.base.ts
@@ -1,15 +1,23 @@
 import { Observable } from 'rxjs';
-import { HttpResponseBase } from '@angular/common/http';
+import { HttpHeaders, HttpRequest, HttpResponseBase } from '@angular/common/http';
+
+export interface NSwagRequest {
+  headers: HttpHeaders;
+}
 
 export class BaseClient {
 
-  protected transformOptions(options: HttpResponseBase): Promise<HttpResponseBase> {
+  protected transformOptions<T extends NSwagRequest>(request: T): Promise<T> {
 
     const token = localStorage.getItem('token');
     if (token != null) {
-      options.headers.append('Authorization', 'Bearer ' + token);
+      const headers: HttpHeaders = request.headers.append('Authorization', 'Bearer ' + token);
+      return Promise.resolve({...request, headers: headers });
+      
+    } else {
+      return Promise.resolve(request);
     }
-    return Promise.resolve(options);
+    
   }
 
   protected transformResult(
